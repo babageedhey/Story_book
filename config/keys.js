ba1f9b9d46c57736c2510d5a6b5bacabfd48a600
babageedhey <babageedhey@gmail.com>
2018-03-23 22:11:31 +0100

    final deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb://geedhey:Admin_01@ds119049.mlab.com:19049/storybook',
-    googleClientID: '124677271334-d0e36np5gtecr0jc70lr61v2v55jn8ag.apps.googleusercontent.com',
-    googleClientSecret: 'XVZ22wqFwP46pyIZf8t0Alzr'
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./keys_prod');
+} else {
+    module.exports = require('./keys_dev');
 }
